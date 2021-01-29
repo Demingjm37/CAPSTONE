f51cdf69a34fa468f05e24263cbfb7d15e171906
Joseph Deming <=>
2021-01-29 18:06:27 -0500

    Makes calls to UpdatePlayer, updates deltaTime, and draws the player and map

--- src/main.c
+++ src/main.c
@@ -1,24 +1,35 @@
 #include "../inc/functions.h"
 
+// current map for testing and placing
+EnvItem envItems [] = {
+    {{0, SCREEN_HEIGHT - 300 , 3800, 50}, BROWN, true}
+};
+
 int main() {
 
     Entity player = { 0 };
     CreatePlayer(&player);
 
-    EnvItem platform = { 0 };
-    platform.hitBox = (Rectangle) {0, SCREEN_HEIGHT - 300 , 3800, 50};
-    platform.color = BROWN;
-    platform.blocking = YES;
-
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Our game");
+
+    int envItemsLength = sizeof(envItems) / sizeof(envItems[0]);
+    float deltaTime = 0;
 
     while(!WindowShouldClose()) {
 
         ClearBackground(WHITE);
 
+        deltaTime = GetFrameTime();
+        UpdatePlayer(&player, envItems, envItemsLength, deltaTime);
+
         BeginDrawing();
             DrawRectangleRec(player.hitBox, player.color);
-            DrawRectangleRec(platform.hitBox, platform.color);
+            DrawText(TextFormat("Player X velocity: %f", player.velocity.x), 0, 0, 20, BLACK);
+
+            for (int i = 0; i < envItemsLength; i ++) {
+                DrawRectangleRec(envItems[i].hitBox, envItems[i].color);
+            }
+
         EndDrawing();
     }
 
