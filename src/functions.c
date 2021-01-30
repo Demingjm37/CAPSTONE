8698f44247be5806c5aa5175e72aca13af0ba404
Joseph Deming <=>
2021-01-29 19:52:29 -0500

    Fixed issues with player vertical and horizontal acceleration. Implemented camera to track player

--- src/functions.c
+++ src/functions.c
@@ -6,8 +6,6 @@ float speedMult = 1.0f;
 
 void UpdatePlayer(Entity *player, EnvItem *envItems, int envItemsLength, float deltaTime) {
 
-    bool isGrounded = false;
-
     float horizontal = IsKeyDown(KEY_D) - IsKeyDown(KEY_A);
     int initJumpVelocity = sqrtf(2 * GRAVITY * player->jumpHeight);
 
@@ -17,19 +15,10 @@ void UpdatePlayer(Entity *player, EnvItem *envItems, int envItemsLength, float d
     player->velocity.x += horizontal * player->speed * deltaTime;
     player->velocity.y += GRAVITY * deltaTime;
 
-    //Determine if player can jump
-    if (isGrounded) {               // Player is currently on the ground
-        player->velocity.y = 0;     // Velocity is 0 so player doesn't move up or down
-        player->canJump = true;     // and is allowed to jump
-    } else {                        
-        player->canJump = false;    // Player is not on the ground, set to false
-                                    // so they can't jump while mid air
-    }
-
     // velocity = sqrt(vx^2 + vy^2)
     float velocity = sqrtf(powf(player->velocity.x, 2) + powf(player->velocity.y, 2));
 
-    // check of new velocity exceeds max velocity
+    // check if new velocity exceeds max velocity
     if (velocity > MAX_VELOCITY) {
         Vector2 temp = player->velocity;
         player->velocity = Vector2Scale(temp, (MAX_VELOCITY / velocity));
@@ -59,7 +48,8 @@ void UpdatePlayer(Entity *player, EnvItem *envItems, int envItemsLength, float d
         player->canJump = true;                                                         // player is on the ground so they should be allowed to jump
     } else {
         player->velocity.y = player->velocity.y / (1 + FRICTION * deltaTime);           // player is not colliding, v_new = v_old / ( 1 friction_const * dt)
-        player->hitBox.y += player->velocity.y;                                         // increase players y with the players new y velocity
+        player->hitBox.y += player->velocity.y;                                         // increase players y position with the players new y velocity
+        player->canJump = false;                                       
     }
 
     // determine if jumping
@@ -77,12 +67,40 @@ void UpdatePlayer(Entity *player, EnvItem *envItems, int envItemsLength, float d
     player->hitBox.x += player->velocity.x;
 }
 
+void UpdateCameraCenter(Camera2D *camera, Entity *player, EnvItem *envItems, int width, int height) {
+    // reset camera position
+    camera->target = (Vector2){player->hitBox.x, player->hitBox.y};
+    camera->offset = (Vector2){width / 2, height / 2};
+
+    float minX = 0, minY = 150, maxX = 3800, maxY = 5000;
+
+    EnvItem temp = envItems[0]; //first element in the map will be used for setting the bounds of the camera
+    // set min and max 
+    minX = fminf(temp.hitBox.x, minX); // sets the smaller of the two to minX
+    maxX = fmaxf(temp.hitBox.x, maxX); // sets the larger of the two to maxX
+
+    Vector2 max = GetWorldToScreen2D((Vector2){maxX, maxY}, *camera); // sets the maximum position for the camera
+    Vector2 min = GetWorldToScreen2D((Vector2){minX, minY}, *camera); // sets the minimum position for the camera
+
+    if (max.x < width) camera->offset.x = width - (max.x - width / 2); // if max.x is less than width set the x offset to w - (max - w/2)
+    if (min.x < 0) camera->offset.x = width / 2 - min.x; // if min position is less than 0 set the offset to (w / 2) - min
+
+    camera->offset.y = height / 2 - min.y; //nothing special for the y-offset. Set to half of the screen height - the minimum y
+
+}
+
 void CreatePlayer(Entity *player) {
-    //todo: move to create player function
     player->hitBox = (Rectangle) {10, SCREEN_HEIGHT - 350, PLYR_SZ, PLYR_SZ};
     player->velocity = (Vector2) {0,0};
     player->color = GREEN;
     player->speed = DFLT_SPD;
     player->jumpHeight = DFLT_JMP_HT;
     player->canJump = YES;
+}
+
+void CreateCamera(Camera2D *camera, Entity *player, int width, int height) {
+    camera->target = (Vector2){player->hitBox.x, player->hitBox.y};
+    camera->offset = (Vector2){width / 2, height / 2};
+    camera->rotation = 0.0f;
+    camera->zoom = 1.0f;
 }
