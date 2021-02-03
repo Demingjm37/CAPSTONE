8a43593912c11891c6d672788974f4396ee0cbd3
Joseph Deming <=>
2021-02-03 18:10:18 -0500

    Implemented Reset key 'R' for quick game resets. Reworked how environment items are drawn

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
+/**
+ * main.c
+ */
+
 #include "../inc/functions.h"
 
 // current map for testing and placing
 EnvItem envItems [] = {
-    {{0, SCREEN_HEIGHT, 3800, 150 }, BROWN, true}
+    {{0, SCREEN_HEIGHT, 3800, 150 }, BROWN, true, 0, false},
+    {{250, SCREEN_HEIGHT - ITEM_SZ * 2, ITEM_SZ, ITEM_SZ}, GREEN, false, 5, false}, //speed boost
+    {{100, SCREEN_HEIGHT - ITEM_SZ * 2, ITEM_SZ, ITEM_SZ}, BLUE, false, 6, false}, //jump boost
+    {{150, SCREEN_HEIGHT - ITEM_SZ * 2, ITEM_SZ, ITEM_SZ}, RED, false, 7, false},  //heart
+    {{200, SCREEN_HEIGHT - ITEM_SZ * 2, ITEM_SZ, ITEM_SZ}, GOLD, false, 8, false}, //coin
+    {{3500, SCREEN_HEIGHT - 50, 100, 200}, BLACK, false, 10, false} //goal post
 };
 
 /**
@@ -39,17 +48,19 @@ int main() {
     // Loop runs as long as the window
     // isn't set to close.
     while(!WindowShouldClose()) {
-
+        if (IsKeyPressed(KEY_R)) ResetGame(&player, envItems, envItemsLength);
         // update delta time, player, and camera
         deltaTime = GetFrameTime();
         UpdatePlayer(&player, envItems, envItemsLength, deltaTime);
         UpdateCameraCenter(&camera, &player, envItems, SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    
+
         // begin drawing the window
         BeginDrawing();
             // reset the window and set background to white
             ClearBackground(WHITE);
-
+            if (DEBUG) Debug(&player);
             // used to initialize 2d mode with the camera 
             BeginMode2D(camera);
                 // draw the players hitbox
@@ -59,13 +70,13 @@ int main() {
                 
                 // Loop through map array and draw each items hitbox
                 for (int i = 0; i < envItemsLength; i ++) {
-                    DrawRectangleRec(envItems[i].hitBox, envItems[i].color);
+                    if (!envItems[i].used) DrawRectangleRec(envItems[i].hitBox, envItems[i].color);
                 }
             EndMode2D();
 
             //This is simply for debugging
             //Todo: replace with helper function for debug uses
-            DrawText(TextFormat("Player X velocity: %f", player.velocity.x), 0, 0, 20, BLACK);
+            
         EndDrawing();
     }
 
