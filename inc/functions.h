afbb2cb377fbcd5bd0d517012567e17615f8aac7
Joseph Deming <=>
2021-02-03 18:11:28 -0500

    Added function prototypes for ResetGame() Debug() Debug funtion implemented in functions.c along with DEBUG macro for displaying info when requested

--- inc/functions.h
+++ inc/functions.h
@@ -20,6 +20,9 @@
 #define DFLT_SPD 1.5f
 #define DFLT_SPRNT_SPD 4.0f
 #define PLYR_SZ 50.0f
+#define ITEM_SZ 25.0f
+
+#define DEBUG true //set to true for debug info
 
 //for fun
 #define YES 1
@@ -32,17 +35,39 @@ typedef struct Entity {
     bool canJump; //used to prevent double jumps
     float speed;  //for setting the players speed
     float jumpHeight; //for setting the players jump height
+    int coins;
 } Entity;
 
 typedef struct EnvItem {
     Rectangle hitBox;
     Color color;
     bool blocking;
+    short id;
+    bool used;
 } EnvItem;
 
 void UpdatePlayer(Entity *player, EnvItem *envItems, int envItemsLength, float deltaTime);
 void UpdateCameraCenter(Camera2D *camera, Entity *player, EnvItem *envItems, int width, int height);
 void CreatePlayer(Entity *player);
 void CreateCamera(Camera2D *camera, Entity *player, int width, int height);
+void ResetGame(Entity *player, EnvItem *envItems, int envItemsLength);
+void Debug(Entity *player);
+
+/**
+ * Environtment Items IDs:
+ *  This is a list of all IDs for future reference when checking collision
+ *  
+ *  0 - basic platform 
+ *  1 - raised platform
+ *  2 - ice platform (implement in future: refactor physics)
+ *  3 - fire
+ *  4 - spikes/barbedwire
+ *  5 - speed boost
+ *  6 - jump boost
+ *  7 - heart
+ *  8 - coin
+ *  9 - reserved
+ * 10 - goal post
+ */
 
 #endif
