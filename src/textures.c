cc284d6c3fa306d6e2116e346aceaf946a4278fe
Joseph Deming <=>
2021-02-10 23:02:19 -0500

    Implemented functions for loading textures, and returning textures

--- src/textures.c
+++ src/textures.c
@@ -8,6 +8,13 @@ Texture2D* LoadTextures() {
         LoadTexture("assets/background/parallax-mountain-trees.png"),
         LoadTexture("assets/background/parallax-mountain-foreground-trees.png")
     };
+    return &textures;
+}
 
-    return textures;
+void UnloadTextures(Texture2D *textures) {
+    int length = sizeof(textures) / sizeof(textures[0]);
+
+    for (int i = 0; i < length; i++) {
+        UnloadTexture(textures[i]);
+    }
 }
