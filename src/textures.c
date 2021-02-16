278fc223cc3bf44c338b433b622e1287670fbeca
Joseph Deming <=>
2021-02-15 23:21:58 -0500

    Fixed warnings

--- src/textures.c
+++ src/textures.c
@@ -1,18 +1,21 @@
 #include "../inc/functions.h"
 
 Texture2D* LoadTextures() {
-    Texture2D textures [] = {
-        LoadTexture("assets/background/parallax-mountain-bg.png"),              
-        LoadTexture("assets/background/parallax-mountain-montain-far.png"),
-        LoadTexture("assets/background/parallax-mountain-mountains.png"),
-        LoadTexture("assets/background/parallax-mountain-trees.png"),
-        LoadTexture("assets/background/parallax-mountain-foreground-trees.png")
+
+    Texture2D *textures = malloc(sizeof(Texture2D) * 5);
+
+    textures = (Texture2D[5]){
+        LoadTexture("../assets/background/parallax-mountain-bg.png"),              
+        LoadTexture("../assets/background/parallax-mountain-montain-far.png"),
+        LoadTexture("../assets/background/parallax-mountain-mountains.png"),
+        LoadTexture("../assets/background/parallax-mountain-trees.png"),
+        LoadTexture("../assets/background/parallax-mountain-foreground-trees.png")
     };
-    return &textures;
+    return textures;
 }
 
 void UnloadTextures(Texture2D *textures) {
-    int length = sizeof(textures) / sizeof(textures[0]);
+    int length = 5;
 
     for (int i = 0; i < length; i++) {
         UnloadTexture(textures[i]);
