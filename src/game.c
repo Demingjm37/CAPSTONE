2f559239f6f9103c798c397095d190457657b6d2
Joseph Deming <=>
2021-02-22 20:45:36 -0500

    Begun work on textures, 2d parallax background, player sprite, and platform textures are drawn

--- src/game.c
+++ src/game.c
@@ -13,9 +13,10 @@ EnvItem envItems[] = {
     {{750, SCREEN_HEIGHT, PLTFRM_SZ, PLTFRM_SZ}, BROWN, true, 0, false},
     {{800, SCREEN_HEIGHT - 75, 75, 100}, RED, true, 1, false},
     {{825, SCREEN_HEIGHT - 100 - ITEM_SZ * 2, ITEM_SZ, ITEM_SZ}, GOLD, false, 8, false},
-    {{1100, SCREEN_HEIGHT, PLTFRM_SZ - 50, PLTFRM_SZ}, BROWN, true, 0, false},
+    {{1100, SCREEN_HEIGHT, PLTFRM_SZ, PLTFRM_SZ}, BROWN, true, 0, false},
     {{1135, SCREEN_HEIGHT - ITEM_SZ * 2, ITEM_SZ, ITEM_SZ}, GOLD, false, 8, false},
-    {{1300, SCREEN_HEIGHT, PLTFRM_SZ * 2, PLTFRM_SZ}, BROWN, true, 0, false},
+    {{1300, SCREEN_HEIGHT, PLTFRM_SZ, PLTFRM_SZ}, BROWN, true, 0, false},
+    {{1450, SCREEN_HEIGHT, PLTFRM_SZ, PLTFRM_SZ}, BROWN, true, 0, false},
     {{1500, SCREEN_HEIGHT - 50, 100, 200}, BLACK, false, 10, false}
 };
 
@@ -42,6 +43,19 @@ void PlayGame() {
     Camera2D camera = { 0 };
     CreateCamera(&camera, &player, SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    Texture2D bg_textures [] = {
+        LoadTexture("assets/background/parallax-mountain-bg.png"),
+        LoadTexture("assets/background/parallax-mountain-montain-far.png"),
+        LoadTexture("assets/background/parallax-mountain-mountains.png"),
+        LoadTexture("assets/background/parallax-mountain-trees.png"),
+        LoadTexture("assets/background/parallax-mountain-foreground-trees.png")
+    };
+
+    Texture2D playerTex = LoadTexture("assets/player/player-newest.png");
+
+
+    Texture2D grass = LoadTexture("assets/map/grass.png");
+
     int envItemsLength = sizeof(envItems) / sizeof(envItems[0]);
     float deltaTime = 0;
 
@@ -59,17 +73,21 @@ void PlayGame() {
 
             // reset the window and set background to white
             ClearBackground(WHITE);
+            DrawBackground(bg_textures, player, camera); // draw this outside of the camera to prevent issues with how the image is drawn
 
             // used to initialize 2d mode with the camera 
             BeginMode2D(camera);
                 // draw the environment items
                 for (int i = 0; i < envItemsLength; i ++) {
-                   if (!envItems[i].used) DrawRectangleRec(envItems[i].hitBox, envItems[i].color);
+                   //if (!envItems[i].used) DrawRectangleRec(envItems[i].hitBox, envItems[i].color);
+                   if (envItems[i].id == 0) DrawTextureEx(grass, (Vector2){envItems[i].hitBox.x, envItems[i].hitBox.y}, 0.0f, 1.0f, WHITE);
+                   else if (!envItems[i].used) DrawRectangleRec(envItems[i].hitBox, envItems[i].color);
                 }
-
-                //update and draw the player
-                UpdatePlayer(&player, envItems, envItemsLength, deltaTime);
                 if (DEBUG) DrawRectangleRec(player.hitBox, player.color);
+                DrawTextureEx(playerTex, (Vector2){player.hitBox.x, player.hitBox.y}, 0.0f,1.0f, WHITE);
+                UpdatePlayer(&player, envItems, envItemsLength, deltaTime);
+                
+
 
             EndMode2D();
 
@@ -78,6 +96,53 @@ void PlayGame() {
             if (DEBUG) Debug(&player);
             
         EndDrawing();
+
     }
+        UnloadTexture(grass);
+        UnloadTextures(bg_textures);
+
+}
+
+/**
+ * DrawBackground
+ * --------------
+ * 
+ * Draws the images and handles the
+ * animation of the background to generate
+ * a 2d parallax.
+ * 
+ * @param bg_textures - array of 2d textures used in the background
+ * @param player - used to provide new positions for the background scrolling
+ * @param camera - used to provide a stationary position for background images that shouldn't move
+ * 
+ * @return none - returns nothing
+ */
+void DrawBackground(Texture2D *textures, Entity player, Camera2D camera) {
+ 
+    // these are the rates that each layer will move
+    float scrollingBack  = 0.05f,
+          scrollingMid   = 0.1f,
+          scrollingFront = 0.5f;
+    
+    // Draw the sky
+    DrawTextureEx(textures[0], (Vector2){0, 0}, 0.0f, 6.0f, WHITE);
+
+    // Draw the farthest mountain
+    DrawTextureEx(textures[1], (Vector2){scrollingBack * -player.hitBox.x, 125}, 0.0f, 4.0f, WHITE);
+    DrawTextureEx(textures[1], (Vector2){textures[1].width * 2 + scrollingBack * -player.hitBox.x + 20, 125}, 0.0f, 4.0f, WHITE);
+
+    // Draw the closets mountains
+    DrawTextureEx(textures[2], (Vector2){scrollingMid * -player.hitBox.x, 125}, 0.0f, 4.0f, WHITE);
+    DrawTextureEx(textures[2], (Vector2){textures[2].width * 2 + scrollingMid * -player.hitBox.x, 125}, 0.0f, 4.0f, WHITE);
+
+
+    // Draw the farthest trees
+    DrawTextureEx(textures[3], (Vector2){scrollingFront * -player.hitBox.x, -50}, 0.0f, 5.0f, WHITE);
+    DrawTextureEx(textures[3], (Vector2){textures[3].width * 2 + scrollingFront * -player.hitBox.x, -50}, 0.0f, 5.0f, WHITE);
+
+    // Draw the foreground trees, these move with the map itself so they don't change at a separate rate
+    DrawTextureEx(textures[4], (Vector2){0, -SCREEN_HEIGHT/4}, 0.0f, 6.0f, WHITE);
+    DrawTextureEx(textures[4], (Vector2){textures[4].width * 4 , -SCREEN_HEIGHT/2}, 0.0f, 6.0f, WHITE);
+    DrawTextureEx(textures[4], (Vector2){textures[4].width * 8 , -SCREEN_HEIGHT/2}, 0.0f, 6.0f, WHITE);
 
 }
