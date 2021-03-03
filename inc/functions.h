48cae76364548a6243c4c72c727238f267abc373
Joseph Deming <=>
2021-03-03 13:07:22 -0500

    Minor Changes

--- inc/functions.h
+++ inc/functions.h
@@ -47,7 +47,7 @@ Version: alpha-0.1.3
 #define MAX_CMA_X 1600
 #define MAX_CMA_Y 5000
 
-#define DEBUG true //set to true for debug info
+#define DEBUG false //set to true for debug info
 
 //for fun
 #define YES 1
--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
  * @author Joseph Deming
  * @author Hunter Craig
  * 
- * @version 0.1.3
+ * @version 0.1.4
  */
 
 
